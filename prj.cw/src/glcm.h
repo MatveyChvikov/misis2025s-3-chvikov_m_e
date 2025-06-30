2f3eaf0bdda56be9680d6994e11a9bb66635bf80
MatveyChvikov <114420293+MatveyChvikov@users.noreply.github.com>
2025-06-30 17:37:57 +0300

    Update glcm.h

--- prj.cw/src/glcm.h
+++ prj.cw/src/glcm.h
@@ -3,45 +3,39 @@
 
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <string>
 
 class GLCMAnalyzer {
 public:
-
     static std::string interpretResult(double contrast, double correlation);
 
-    // Конструктор
     GLCMAnalyzer();
-
-    // Деструктор
     ~GLCMAnalyzer();
 
-    // Загрузка изображения
     bool loadImage(const std::string& imagePath);
+    cv::Mat calculateGLCM(const cv::Mat& image);
 
-    // Вычисление GLCM матрицы
-    cv::Mat calculateGLCM(const cv::Mat& image, int distance = 1, int angle = 0);
-
-    // Вычисление контраста (формула из изображения)
     double getContrastFeatureValue(const cv::Mat& glcm);
-
-    // Вычисление корреляции (формула из изображения)
     double getCorrelationFeatureValue(const cv::Mat& glcm);
 
-    // Получение загруженного изображения
     cv::Mat getImage() const { return image_; }
-
-    // Получение последней вычисленной GLCM матрицы
     cv::Mat getGLCM() const { return glcm_; }
+
+    // Методы для настройки параметров
+    void setAngles(const std::vector<int>& angles) { angles_ = angles; }
+    void setDistance(int distance) { distance_ = distance; }
 
 private:
     cv::Mat image_;
     cv::Mat glcm_;
+    std::vector<int> angles_;   // Углы в градусах (0, 45, 90, 135)
+    int distance_ = 1;          // Расстояние (радиус)
 
     // Вспомогательные функции
     void normalizeGLCM(cv::Mat& glcm);
     std::pair<double, double> calculateMeans(const cv::Mat& glcm);
-    std::pair<double, double> calculateStandardDeviations(const cv::Mat& glcm, 
-                                                          double meanX, double meanY);
+    std::pair<double, double> calculateStandardDeviations(const cv::Mat& glcm,
+        double meanX, double meanY);
 };
 
 #endif // GLCM_H
